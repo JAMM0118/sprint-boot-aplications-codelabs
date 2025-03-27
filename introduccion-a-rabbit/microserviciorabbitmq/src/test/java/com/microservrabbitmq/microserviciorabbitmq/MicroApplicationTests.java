4d10f859a0ad8f730eb3ad092b9337e6bdb18157
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2025-03-27 11:54:51 -0500

    add project using docker and rabbitMQ

---
+++ introduccion-a-rabbit/microserviciorabbitmq/src/test/java/com/microservrabbitmq/microserviciorabbitmq/MicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservrabbitmq.microserviciorabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
